ccf4d6e68d41263747849af1b15169981018ce19
Darcy Keys <dkeys8722@gmail.com>
2022-05-03 10:38:41 +1000

    added among us trap remix, made months not case sensitive

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from guizero import *
 from datetime import datetime
+import webbrowser
 app = App("age calculator")
 monthDictionary = {
-    "January" : 1,
-    "February":2,
-    "March":3,
-    "April":4,
-    "May":5,
-    "June":6,
-    "July":7,
-    "August":8,
-    "September":9,
-    "October":10,
-    "November":11,
-    "December":12
+    "january" : 1,
+    "february":2,
+    "march":3,
+    "april":4,
+    "may":5,
+    "june":6,
+    "july":7,
+    "august":8,
+    "september":9,
+    "october":10,
+    "november":11,
+    "december":12
 }
-def reset():
+def close():
+    webbrowser.open("https://www.youtube.com/watch?v=grd-K33tOSM")
     app.destroy()
-    app.display()
 def age_calculate():
     year = datetime.now().year - int(input_year.value)
     if len(input_month.value) >= 3:
-        month = datetime.now().month - int(monthDictionary[input_month.value])
+        month = datetime.now().month - int(monthDictionary[input_month.value.lower()])
     else:
         month = datetime.now().month - int(input_month.value)
     day = datetime.now().day - int(input_day.value)
@@ -46,5 +47,5 @@ input_month = TextBox(app)
 message = Text(app,text="Input Day Born")
 input_day = TextBox(app)
 button = PushButton(app,text = "calculate age",command= age_calculate)
-button = PushButton(app,text = "reset",command = reset)
+button = PushButton(app,text = "commit a major act of terrorism",command = close)
 app.display()
